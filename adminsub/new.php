67f758a8f11ff27efd999d2621de937b47026d09
liderbug <chucka@blackforest-co.com>
2023-09-09 19:32:43 -0600

    Update new.php

--- adminsub/new.php
+++ adminsub/new.php
@@ -24,8 +24,30 @@ echo "
 <tr><td><hr></td><td><hr></td></tr>
 <tr><td>Note</td><td><input type=text name=desc size=40 ></td></tr>
 <tr><td>Contact</td><td><input type=text name=contact size=40 ></td></tr>
-<tr><td>Location</td><td><input type=text name=loc size=40 value=Club> * Club, Pavilion, C&P, N40</td></tr>
-<tr><td>Type</td><td><input type=text name=type size=40 value=Paid> * Paid, Discount, Free, </td></tr>
+<tr><td>Location</td><td>
+<select name=location>
+<option value=Club selected>Club
+<option value=Pavioiln>Pavilion
+<option value=Club+Pavilion>Club & Pavioion
+<option value=N40>N40
+<option value=Parking>Parking
+<option value=Other>Other
+</select>
+ * Club, Pavilion, C&P, N40</td></tr>
+<tr><td>Type</td><td>
+<select name=rtype id=rtype>
+<option value=Discount selected>Discount
+<option value=Paid>Paid
+<option value=Free>Free
+<option value=Sponsored>Sponsored
+<option value=Other>Other
+</select>
+
+ * Paid, Discount, Free, </td></tr>
+
+ `location` enum('Club','Club+Pavilion','Pavilion','N40','Parking') DEFAULT 'Club',
+     `type` enum('Paid','Discount','Free','Sponsored') DEFAULT NULL,
+
 <!--tr><td>ModDate</td><td>
 <input type='datetime-local' name='mod'>
 </td></tr-->
@@ -40,7 +62,7 @@ $year = substr($indate, 0,4);
 $month = substr($indate, 5,2);
 echo "<pre></center>";
 
-$list = array ('date', 'dur', 'end', 'rep', 'name', 'desc', 'contact', 'loc', 'type', 'moddate', 'who','cat');
+$list = array ('date', 'dur', 'end', 'rep', 'name', 'desc', 'contact', 'location', 'rtype', 'moddate', 'who','cat');
 foreach ($list as $l) { $$l = $_POST [$l]; }
 $name = str_replace ("'", "\\'", $name);
 $date = strtotime($date);
@@ -52,7 +74,7 @@ $q1i = "insert into cal_entry values (0, $date, $dur, $end, '$rep', '$name', '$c
 $r1 = mysqli_query ($newdb, $q1i);
 $id = mysqli_query ($newdb, "select max(id) from cal_entry;");
 $id1 = mysqli_fetch_row ($id);
-$q2i = "insert into cal_misc values ($id1[0], '$desc', '$contact', '$loc', '$type', $moddate, '$who[0]');";
+$q2i = "insert into cal_misc values ($id1[0], '$desc', '$contact', '$location', '$rtype', $moddate, '$who[0]');";
 $r2 = mysqli_query ($newdb, $q2i);
 echo "<input type=button value=CAL onclick=window.location.href=index.php>";
 echo "<meta http-equiv='refresh' content='0;url=index.php?year=$year&month=$month' />";
