05c52e77ef2ed73de7242170ee4824284bdf9154
liderbug <chucka@blackforest-co.com>
2023-09-24 08:17:25 -0600

    Update install.bash

--- install.bash
+++ install.bash
@@ -8,8 +8,7 @@ hiddir=`tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo ''`
 echo $hiddir
 mv adminsub .$hiddir
 mv '[dot]dbconnect.php' .dbconnect.php
-sed -i 's/44foo4fee5/'.$hiddir'/' index.php
-sed -i 's/foo4fee5/'$hiddir'/' index.php
+sed -i 's/hidsubdir/'$hiddir'/' index.php
 sed -i 's/HOSTNAME/'$HOSTNAME'/' index.php
 
 echo -n "Hostname?  " ; read ans
