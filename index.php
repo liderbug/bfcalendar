af859b8646a20a72c3b38af1673d2174a2a69a04
liderbug <chucka@blackforest-co.com>
2023-10-13 08:40:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,70 @@
+
 <head>
 <script type="text/javascript">
 function selcat(val) {
   var val;
-  var  url = `/webcal/index.php?setcat=${val}`;
+  var  url = `[yourwebsite]webcal/index.php?setcat=${val}`;
   window.location.href = url;
 }
 </script>
 <style>
 .text-right { text-align: right; }
-td { color: black; font-size: 12px; vertical-align: top; }
+td { color: black; font-size: 12px; vertical-align: top; }                  
+
 </style>
 </head>
 
 <body bgcolor=#AFA>
 <?php
+
 function lilo ()
 {
   $cwd = getcwd();
-  if ( strstr($cwd, "hidsubdir") )
+  if ( strstr($cwd, "TOPPTkM8NkxLi") )
   { 
     echo "<p valign=top class='text-right'><a href=../index.php>logout</a></p>";
   } else {
-    echo "<p class='text-right'><a href=hidsubdir>admin</a></p>";
+    echo "<p class='text-right'><a href=[hiddenadmindir]>admin</a></p>";
   }
 }
 
-function do_month() { return (-1); }
+function do_month ( $ots, $x, $mo, $yr)
+{
+  $ts = strtotime ("$x", mktime(0,0,0,$mo,1,$yr)); 
+  $day = date ('d', $ts);
+  if ($day == 22)
+    $day -= 7;
+
+  $dst =  (date ('I', $ts))? 1:0; 
+  return ("$ts,$day");
+}
 
 function do_week($x, $dow, $rmo, $ryr)
 {
     $name = array( '', 'first', 'second', 'third', 'fourth', 'fifth');
     $wom  = $name[$x];
-    return (date('j', strtotime("$wom $dow of $rmo $ryr")));
+    $ts = date('j', strtotime("$wom $dow of $rmo $ryr"));
+    return ($ts);
 }
 
 function dow($ts)
 {
-    $down = date("w", $ts);
     $name = array( "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
     return ($name[date('w', $ts)]);
 }
 
 function do_day($day, $darray, $date, $today)
 {
-   $k1='';
-   $cwd = getcwd();
-   $pw = ( strstr($cwd, "hidsubdir") ) ? 1:0;
+    $k1 = '';
+        $cwd = getcwd();
+    $pw = ( strstr($cwd, ".TOPPTkM8NkxLi") ) ? 1:0;
     if ($day > 0) {
         $bgc = ($date == $today) ? "#fcf37f" : "white";
         echo "<td width=14% valign=top bgcolor=$bgc>$day";
-		if ($pw == 1) { 
-			echo "<a href=new.php?date=$date> Add</a>";
-		}
-		echo "<br>\n";
+                if ($pw == 1) { 
+                        echo "<a href=new.php?date=$date> Add</a>";
+                }
+                echo "<br>\n";
         $block = 0;
         foreach ($darray as $events) {
             $i = explode(",", $events);
@@ -77,18 +89,19 @@ function do_day($day, $darray, $date, $today)
             }
         }
     } else { # a non-this-month table cell
-        echo "<td width=100 height=100 valign=top bgcolor=pink>";
+        echo "<td width=100 height=100 valign=top bgcolor=pink> </td>\n";
     }
     echo "</td>\n";
 }
 
 # ----- main () -----
+$Home = "[yourhost.com]/webcal"; # set this to "yourhost.com/[pwd]"
 date_default_timezone_set('America/Denver');
 
 if ( isset ($_GET['month']))
-{ 
+{
   $month = $_GET['month'];
-  $year = $_GET['year']; 
+  $year = $_GET['year'];
 } else {
   $adom = $_POST['adom'];
   $a = explode (",", $adom);
@@ -138,9 +151,9 @@ $rmo = date('F', strtotime($mw));
 echo "<table border=0 width=95% align=center>
   <tr height=100px>
     <td align=left valign=middle width=20%>
-      <input type=image src=/images/leftarrow.gif alt=Submit value=prev name=monthm />
+      <input type=image src=https://$Home/images/leftarrow.gif alt=Submit value=prev name=monthm />
     </td>
-    <td align=center ><H2><br><b>$m2</b></H2>";
+    <td align=center >NEW CALENDAR<H2><br><b>$m2</b></H2>";
 # category 1 is Paid & Sponsored. Cat 2 is Free or Discounted that can be bumped for cat 1 events
 if ($setcat == 0) { $sel0='selected'; }
 if ($setcat == 1) { $sel1='selected'; }
@@ -154,7 +167,7 @@ echo "<select onchange=selcat(value)>
 </td><td align=right valign=middle width=20%>";
 echo "<table border=0 height=100%><tr><td valign=top>";
 lilo ();
-echo "</td></tr><tr><td valign=top><input type=image src=/images/rightarrow.gif alt=Submit value=next name=monthp />";
+echo "</td></tr><tr><td valign=top><input type=image src=https://$Home/images/rightarrow.gif alt=Submit value=next name=monthp />";
 echo "</td></tr></table>";
 echo "</td></tr>";
 echo "<tr height=100px><td colspan=3>";
@@ -162,10 +175,10 @@ echo "<tr height=100px><td colspan=3>";
 exec("cal $month $year | tr -d '_' | tr -d '\b'", $month1); # Use system "cal" command
 $week = array_slice($month1, 2);
 $x    = substr($week[0], -1);
-$day  = $x - 6;
+$day  = $x-6;
 
 $last  = ($week[5]) ? substr($week[5], -2) : substr($week[4], -2);
-#echo "L $last ";
+
 $wpm   = (strlen($week[5]) == 0) ? 4 : 5;
 $today = date('Ymd', time());
 
@@ -186,6 +199,7 @@ switch ($setcat) { # display all or cat 1 or cat 1 events
 
 # once
 $q1 = "select id, date, description, category, duration from cal_entry where repeats = 'once' and date between $som and $eom $getcat order by date desc;";
+#echo "$q1<br>";
 $r1 = mysqli_query($newdb, $q1);
 
 while ($r = mysqli_fetch_array($r1)) {
@@ -195,38 +209,50 @@ while ($r = mysqli_fetch_array($r1)) {
 }
 
 # repeats
+$ZZ = 0;
 $q2   = "select id,date,repeats,description,category,duration from cal_entry where repeats != 'once' and end_date > $som $getcat;";
-#echo "$q2<br>";
 $r1   = mysqli_query($newdb, $q2);
-#$num_rows = mysqli_num_rows($r1);
 $ryr  = $year;
 $rday = date('j', $r[1]);
-while ($r = mysqli_fetch_array($r1)) {
+
+while ($r = mysqli_fetch_array($r1)) 
+{
     $lweek = 0;
     $dow = dow($r[1]);
     $i   = explode(",", $r[2]);
     $lts = 0;
-    for ($x = 1; $x <= count($i) - 1; $x++) {
+    for ($x = 1; $x <= count($i) - 1; $x++) 
+    {
+    $lweek = 0;
         switch ($i[0]) {
             case 'y':
-                if ($r[1] >= $som && $r[1] <= $eom)
-                    $ts = date('j', $r[1]);
+                if ($r[1] >= $som && $r[1] <= $eom) { $ts = date('j', $r[1]); }
                 break;
             case 'm':
-                $ts = do_month(); #thinking ahead
+                  $key = array_search($month, $i); 
+                  if ( $key > 0 )
+                  {
+                    $key = $i[$key];
+                    if ($key == $month)
+                    {
+                      $ret = do_month ( $r[1], $i[1], $month, $year);
+                      $t1 = explode (',', $ret);
+                      $H = $t1[1];
+                      array_push($carray[$t1[1]], "$H|$r[4]|$r[1]|$r[3]|$r[5]|$r[0]");
+                    }
+                    $x=99;
+                  } else {
+                     $x=99;
+                  }
                 break;
             case 'w':
                 $ts = do_week($i[$x], $dow, $rmo, $ryr);
                  if ($ts > $lweek) { $lweek = $ts; $push=1; } else { $push=0;}
+                 $H = date('H', $r[1]);
+                 array_push($carray[$ts], "$H|$r[4]|$r[1]|$r[3]|$r[5]|$r[0]");
                 break;
             default:
-                echo "dow $dow<br>";
                 break;
-        }
-        if ($push)
-        {
-          $H = date('H', $r[1]);
-          array_push($carray[$ts], "$H|$r[4]|$r[1]|$r[3]|$r[5]|$r[0]");
         }
     }
 }
@@ -234,8 +260,8 @@ while ($r = mysqli_fetch_array($r1)) {
 #sort each day by time, category
 for ($n = 0; $n <= $last; $n++)
     sort($carray[$n]);
-
-echo "<table border=1 width=100%><tr height=100px><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>\n";
+#echo "<pre>"; print_r($carray);
+echo "<table 3 border=1 width=100%><tr height=100px><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>\n";
 foreach ($week as $w) {
     $w = trim($w);
     echo "<tr height=100px>\n";
@@ -248,9 +274,9 @@ foreach ($week as $w) {
           $d++;
           do_day(0, $carray, $date, $today, $carray[5]);
         }
-	echo "</tr></table>";
-	echo "<input type=hidden name=adom value='$month,$year,1,2,3'>";
-	echo "</table>";
+        echo "</tr></table>";
+                 echo "<input type=hidden name=adom value='$month,$year,1,2,3'>";
+         echo "</table>";
          echo "<td>
 <tr>
 </table>";
@@ -258,7 +284,9 @@ foreach ($week as $w) {
       }
       $day++;
     }
-    echo "</tr>";
+    echo "</tr>\n";
 }
+
+
 ?>
 </form>
